0095902a2df5933597cd402004e7835716a3d91e
AlphaJR36 <alphajrdev@gmail.com>
2023-04-04 01:15:10 +0100

    1.0.0

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,26 +1,8 @@
-require('dotenv').config();
-const Discord = require('discord.js');
-const SquareCloudAPI = require('@squarecloud/api');
-const api = new SquareCloudAPI(process.env.squareCloudKey)
-
 module.exports = {
     name: "ready",
 
     run: async (client) => {
 
-        const user = await api.getUser(process.env.squareCloudUser)
-
         console.log("\n🤖 " + "| " + "Logado em: " + client.user.username)
-        try {
-
-            if (user.hasAccess()) {
-                console.log("\n🔳 " + "| " + "Square Cloud: Conectada com Sucesso")
-            }
-        } catch (error) {
-            if(error.data.status === 'error' && error.data.code === 'ACCESS_DENIED') {
-                console.log("\n🔳 " + "| " + "Square Cloud: Conectada sem Sucesso")
-            }
-        }
-
     }
 }
